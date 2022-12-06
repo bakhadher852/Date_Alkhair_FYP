178a02c9d29a8b753d460cd578d94a91815b066d
bakhadher <mohammed85270@gmail.com>
2022-12-06 14:50:44 +0800

    sign up page

--- lib/screen/signUp.dart
+++ lib/screen/signUp.dart
@@ -1,20 +1,104 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 class SignUp extends StatefulWidget {
-  const SignUp({super.key});
+  SignUp({super.key});
   static const RouteName = '/SignUpPage';
+
   @override
   State<SignUp> createState() => _SignUpState();
 }
 
 class _SignUpState extends State<SignUp> {
-  @override
+  final userName = TextEditingController();
+  final userOccupation = TextEditingController();
+  final userEmail = TextEditingController();
+  final userPassword = TextEditingController();
+  final confirmPassword = TextEditingController();
+  final userPhone = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
+    print(userEmail.text);
+    print('=====================================');
     var AppbarHeight = AppBar().preferredSize.height;
     final height = MediaQuery.of(context).size.height;
     final width = MediaQuery.of(context).size.width;
+    //Name textFiled
+    Widget buildName() => Padding(
+          padding: EdgeInsets.symmetric(horizontal: height * 0.04),
+          child: TextField(
+            controller: userName,
+            decoration: const InputDecoration(
+              border: UnderlineInputBorder(),
+              labelText: 'Enter your name',
+            ),
+            keyboardType: TextInputType.name,
+            textInputAction: TextInputAction.done,
+          ),
+        );
+    //Occupation textFiled
+    Widget buildOccupation() => Padding(
+          padding: EdgeInsets.symmetric(horizontal: height * 0.04),
+          child: TextField(
+            controller: userEmail,
+            decoration: const InputDecoration(
+              border: UnderlineInputBorder(),
+              labelText: 'Enter your occupation',
+            ),
+            keyboardType: TextInputType.text,
+            textInputAction: TextInputAction.done,
+          ),
+        );
+    //Email textFiled
+    Widget buildEmail() => Padding(
+          padding: EdgeInsets.symmetric(horizontal: height * 0.04),
+          child: TextField(
+            controller: userEmail,
+            decoration: const InputDecoration(
+              border: UnderlineInputBorder(),
+              labelText: 'Enter your email',
+            ),
+            keyboardType: TextInputType.emailAddress,
+            textInputAction: TextInputAction.done,
+          ),
+        );
+    //Password textFiled
+    Widget buildPassword() => Padding(
+          padding: EdgeInsets.symmetric(horizontal: height * 0.04),
+          child: TextField(
+            controller: userPassword,
+            decoration: const InputDecoration(
+              labelText: 'Enter your password',
+              border: UnderlineInputBorder(),
+            ),
+            obscureText: true,
+          ),
+        );
+    //Password textFiled
+    Widget buildConfirmPassword() => Padding(
+          padding: EdgeInsets.symmetric(horizontal: height * 0.04),
+          child: TextField(
+            controller: confirmPassword,
+            decoration: const InputDecoration(
+              labelText: 'Confirm your Password',
+              border: UnderlineInputBorder(),
+            ),
+            obscureText: true,
+          ),
+        ); //Occupation textFiled
+    Widget buildUserPhone() => Padding(
+          padding: EdgeInsets.symmetric(horizontal: height * 0.04),
+          child: TextField(
+            controller: userPhone,
+            decoration: const InputDecoration(
+              border: UnderlineInputBorder(),
+              labelText: 'Enter your Mobile number',
+              hintText: '011*******',
+            ),
+            keyboardType: TextInputType.number,
+            textInputAction: TextInputAction.done,
+          ),
+        );
     return Stack(children: [
       Scaffold(
           appBar: AppBar(
@@ -22,12 +106,29 @@ class _SignUpState extends State<SignUp> {
             backgroundColor: const Color(0xffC1CDED),
           ),
           body: Container(
+            color: const Color(0xffC1CDED),
             height: height,
             width: width,
             child: SingleChildScrollView(
-              //to avoid keyboard bottom overflow
               child: Column(
-                children: [],
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 9, top: height * 0.07),
+                    child: const Text(
+                      'Sign Up',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 40,
+                          color: Color.fromARGB(206, 33, 149, 243)),
+                    ),
+                  ),
+                  buildName(),
+                  buildOccupation(),
+                  buildEmail(),
+                  buildPassword(),
+                  buildConfirmPassword(),
+                  buildUserPhone()
+                ],
               ),
             ),
           )),
---
+++ lib/widget/string_extension.dart
@@ -0,0 +1,13 @@
+extension StringExtensions on String {
+  bool isValidEmail() {
+    return RegExp(
+            r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+        .hasMatch(this);
+  }
+
+//https://stackoverflow.com/questions/16800540/how-should-i-check-if-the-input-is-an-email-address-in-flutter/61512807#61512807
+  bool isWhitespace() => this.trim().isEmpty;
+
+  bool isValidDouble() => double.tryParse(this) != null;
+  bool isValidInt() => int.tryParse(this) != null;
+}
