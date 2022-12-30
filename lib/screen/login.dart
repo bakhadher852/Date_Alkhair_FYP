83852d736d05b810bbcf7bb23412cd34481a86b3
bakhadher <mohammed85270@gmail.com>
2022-12-30 16:38:54 +0800

    added

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -43,7 +43,7 @@ class _LoginState extends State<Login> {
                     height: height * 0.16,
                   ),
                   Padding(
-                    padding: EdgeInsets.only(left: width * 0.17),
+                    padding: EdgeInsets.only(left: width * 0.01),
                     child: buildTitleText('Khair'),
                   ),
                   buildTitleText('Data Sharing'),
